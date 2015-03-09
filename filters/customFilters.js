af055f45b0ec2b04bb40546b43df7ef1f0040f5a
mhagel <mark.hagelberg@gmail.com>
2015-03-09 15:17:51 -0600

    through listing 7-5

--- filters/customFilters.js
+++ filters/customFilters.js
@@ -16,4 +16,31 @@ angular.module("customFilters", [])
                 return data;
             }
         }
+    })
+.filter("range", function ($filter) {  //returns a range of elements from an array, corresponding to a page of products.
+        return function (data, page, size) {
+            if (angular.isArray(data) && angular.isNumber(page) && angular.isNumber(size)) {
+                var start_index = (page - 1) * size;
+                if (data.length < start_index) {
+                    return [];
+                } else {
+                    return $filter("limitTo")(data.splice(start_index).size);
+                }
+            } else {
+                return data;
+            }
+        }
+    })
+.filter("pageCount", function () { //works out how many pages an array can be displayed in and then creates an array with that many numeric values.
+        return function (data, size) {
+            if (angular.isArray(data)) {
+                var result = [];
+                for (var i = 0; i < Math.ceil(data.length / size) ; i++) {
+                    result.push(i);
+                }
+                return result;
+            } else {
+                return data;
+            }
+        }
     });
